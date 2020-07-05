f8714ae5af228e4ac4b1406988abf650e41875b8
Ilia Bozhinov <ammen99@gmail.com>
2020-07-05 17:03:28 +0200

    add ci test

---
+++ source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stdio.h>
+#define X first
+#define Y second
+
+#define _ << " " <<
+#define debug(x) #x << " = " << x
+
+#define ll long long
+#define ull unsigned long long
+#define all(v) v.begin(), v.end()
+
+using pii = std::pair<int, int>;
+using pll = std::pair<ll, ll>;
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
